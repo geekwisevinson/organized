182628102058becf3f7532739ac9ddffe5ad752d
Vinson Fernandez <geekwise.vinson.fernandez@gmail.com>
2016-09-19 17:03:58 -0700

    brain started

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert('hello');
+var links = ["https://github.com/candelibas/awesome-ionic2"];
+
+var ang2li =document.getElementById('ang2');
+
+var creator = function (type,parent){
+	el = document.createElement(type);
+	parent.appendChild(el);
+	return el;
+};
+
+for (var i=links.length;i>0;i--){
+	var el = creator('li',ang2li);
+	el.innerHTML = links[i];
+}
